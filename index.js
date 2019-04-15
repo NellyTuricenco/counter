b158057f9abfa870ce2182332fe1bbd9284e9139
Nelly <turicenco@gmail.com>
2019-04-15 23:03:47 +0300

    Counter_Lesson22_Homework

---
+++ index.js
@@ -0,0 +1,14 @@
+var counter = parseInt(prompt(" Insert the start point of the counter", 0));
+var button = document.getElementById("clickme");
+var startPoint = button.innerHTML = "Click me "+ counter;
+button.onclick = function() {
+  counter += 1;
+  button.innerHTML = "Click me " + counter;
+};
+
+// $ (function(){
+//     var count = counter;
+//     $("#clickme").on("click", function(){
+//     count +=1
+//     });
+//     });
