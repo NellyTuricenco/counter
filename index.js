95d878fd21fc44a8c378ae8fec29873536b131e9
Nelly <turicenco@gmail.com>
2019-04-16 13:11:37 +0300

    Counter_Lesson22_Homework via jQuery

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var counter = parseInt(prompt(" Insert the start point of the counter", 0));
 var button = document.getElementById("clickme");
 var startPoint = button.innerHTML = "Click me "+ counter;
-button.onclick = function() {
-  counter += 1;
-  button.innerHTML = "Click me " + counter;
-};
+// button.onclick = function() {
+//   counter += 1;
+//   button.innerHTML = "Click me " + counter;
+// };
 
-module.exports = {
-  onclick,  
-};
+// module.exports = {
+//   onclick,  
+// };
 
-// $ (function(){
-//     var count = counter;
-//     $("#clickme").on("click", function(){
-//     count +=1
-//     });
-//     });
+$ (function(){
+    $("#clickme").on("click", function(){
+    counter +=1;
+    button.innerHTML = "Click me " + counter;
+    });
+    });
